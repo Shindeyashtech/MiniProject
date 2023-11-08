38d5b03334e8a2ee102b50faed0e0d51946de071
Shindeyashtech <150181654+Shindeyashtech@users.noreply.github.com>
2023-11-08 04:32:23 +0000

    done

--- yash.js
+++ yash.js
@@ -1,1 +1,16 @@
-console.error("its")
+
+var a =50;
+if(a==10){
+    document.write("a is equal to 10")
+}else if(a==15){
+    document.write("a is greater than 15")
+
+}else if(a==20)
+{
+    document.write(" a is equal to 20")
+
+}
+else{
+    document.write("a is not equal to 10,20,15")
+}
+
